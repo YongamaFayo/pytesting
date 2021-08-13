d2f6ec2df63e493ae8ab131ecb4b39cf58bab508
Yongama Patrick Fayo <yongama.fayo@wundermanthompson.com>
2021-08-13 22:14:00 +0200

    first commit

---
+++ greet.py
@@ -0,0 +1,25 @@
+def greet(name):
+    return 'Hello ' + name
+
+
+greet('Yongama') 
+
+
+
+
+# def greet(language):
+#     if language == 'Xhosa':
+#         return 'Molo ' + name
+#     elif language == 'English':
+#         return 'Hello ' + name
+
+#     elif language == 'Afrikaans':
+#         return 'Hallo ' + name
+     
+# name = input('Please enter your name: ')     
+
+
+# print(greet('English'))
+
+
+
---
+++ test_pytest.py
@@ -0,0 +1,8 @@
+import greet
+import pytest
+
+
+def test_greeted():
+    assert greet('Yongama') == 'Hello Yongama'
+
+
